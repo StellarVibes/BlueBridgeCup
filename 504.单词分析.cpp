e2ddb95b96f749fc6c3377056db005d67418c225
qhe32673-arch <qhe32673@gmail.com>
2025-10-27 20:30:17 +0800

    2020 模拟 枚举 省赛

---
+++ 504.单词分析.cpp
@@ -0,0 +1,28 @@
+// 解题思路
+// 1.for(auto &e:s) arr[e-'a']++; 2.if(max<arr[i]) max = arr[i]; ret = i+'a';
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    int max = 0;
+    char ret;
+    cin>>s;
+    int arr[26]={0};
+    for(auto &e:s)
+    {
+        arr[e-'a']++;
+    }
+    for(int i =0;i<26;i++)
+    {
+        if(max<arr[i])
+        {
+            max = arr[i];
+            ret = i+'a';
+        }
+    }
+  cout<<ret<<endl<<max;
+  return 0;
+}
