4ce45ce366847caa47e432180749fe06847161ed
枫叶c <2744235537@qq.com>
2025-12-07 00:43:39 +0800

    2021 省模拟赛

---
+++ 547.平行四边形面积.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int l = 0.0;
+    int h = 0.0;
+    scanf("%d%d", &l, &h);
+    printf("%d", l * h);
+    
+    return 0;
+}
