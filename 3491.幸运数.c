184f35921526671c0d89e7ae2b3410d99c1290e9
枫叶c <2744235537@qq.com>
2025-12-07 00:42:00 +0800

    2023 模拟 枚举 省赛

---
+++ 3491.幸运数.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    /*int count = 0;
+    for (int c = 10; c < 100000000; c++)
+    {
+        int len = 0;
+        int i = 0;
+        int sum = 0;
+        int a = c;
+        int b = c;
+        while (a)
+        {
+            a /= 10;
+            len++;
+        }
+        if (len % 2 == 0)
+        {
+            len /= 2;
+        while (len)
+        {
+            sum += b % 10;
+            b /= 10;
+            len--;
+        }
+        while (b)
+        {
+            sum -= b % 10;
+            b /= 10;
+        }
+        if (sum == 0)
+        {
+            count++;
+            //printf("%d\n",c);
+        }
+        }
+
+    }*/
+    printf("%d", 4430091);
+    
+    return 0;
+}
